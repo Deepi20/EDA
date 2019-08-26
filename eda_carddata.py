22bc4cb5f80acbb354112d6cd8bbe80d23ebde8e
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-08-25 22:59:15 -0500

    Update eda_carddata.py

--- eda_carddata.py
+++ eda_carddata.py
@@ -10,3 +10,15 @@ print(occ)
 
 # Print the ratio of fraud cases
 print(occ / df.shape[0])
+
+def plot_data(X, y):
+	plt.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5, linewidth=0.15)
+	plt.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5, linewidth=0.15, c='r')
+	plt.legend()
+	return plt.show()
+
+# Create X and y from the prep_data function 
+X, y = prep_data(df)
+
+# Plot our data by running our plot data function on X and y
+plot_data(X, y)
