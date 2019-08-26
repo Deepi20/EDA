41b038686c4853a86b82a3021503ee44bcdbc936
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-08-25 22:58:34 -0500

    Create eda_carddata.py

---
+++ eda_carddata.py
@@ -0,0 +1,12 @@
+import pandas as pd
+df = pd.read_csv("creditcard_data.csv")
+
+# Explore the features available in your dataframe
+print(df.info())
+
+# Count the occurrences of fraud and no fraud and print them
+occ = df['Class'].value_counts()
+print(occ)
+
+# Print the ratio of fraud cases
+print(occ / df.shape[0])
