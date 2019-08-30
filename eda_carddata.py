20ecd293081aaaa1c0222dc24baea342012ae1bc
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-08-29 23:30:32 -0500

    Update eda_carddata.py

--- eda_carddata.py
+++ eda_carddata.py
@@ -167,3 +167,5 @@ clf1 = LogisticRegression(class_weight={0:1, 1:15}, random_state=5)
 clf2 = RandomForestClassifier(class_weight={0:1, 1:12}, criterion='gini', max_depth=8, max_features='log2',
                               min_samples_leaf=10, n_estimators=30, n_jobs=-1, random_state=5)
 clf3 = DecisionTreeClassifier(random_state=5, class_weight="balanced")
+ensemble_model = VotingClassifier(estimators=[('lr', clf1), ('rf', clf2), ('dt', clf3)], voting='hard')
+get_model_results(X_train, y_train, X_test, y_test, ensemble_model)
