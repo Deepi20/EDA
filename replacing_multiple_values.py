6dc969e16ece3789742279dc028a0fd8589f8603
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-09-09 23:56:07 -0500

    Update replacing_multiple_values.py

--- replacing_multiple_values.py
+++ replacing_multiple_values.py
@@ -5,4 +5,8 @@ def Snippet_104():
     warnings.filterwarnings("ignore")
     import pandas as pd
     import numpy as np
-    
+    raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
+                'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze'],
+                'age': [42, 52, 36, 24, 73],
+                'preTestScore': [-999, -999, -999, 2, 1],
+                'postTestScore': [2, 2, -999, 2, -999]}
