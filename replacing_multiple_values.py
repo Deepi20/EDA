e5b097c015bb88fdf85366cfbd1290cb2c8841d9
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-09-09 23:55:56 -0500

    Update replacing_multiple_values.py

--- replacing_multiple_values.py
+++ replacing_multiple_values.py
@@ -3,3 +3,6 @@ def Snippet_104():
     print(format('How to replace multiple values in a Pandas DataFrame','*^82'))
     import warnings
     warnings.filterwarnings("ignore")
+    import pandas as pd
+    import numpy as np
+    
